1a6e604bf7a9956d63fe40efc1c52b25d12b8fbf
mjhe <matthenry4@gmail.com>
2017-04-27 08:01:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 (function() {
-  var _geolocation = null
+  var _key = "language-detected"
+  var _language_detected = localStorage.getItem(_key)
   
+  if (_language_detected == null || _language_detected == "") {
+    var _language = window.navigator.languages ? window.navigator.languages[0] : (window.navigator.userLanguage || window.navigator.language)
+    if (_language == null) {
+      _language = "en-US"
+    }
+    _language_detected = _language.toLowerCase()
+    
+    localStorage.setItem(_key, _language_detected)
+  }
+  
+  if (window.location.pathname == "/" && _language_detected != null && _language_detected != "") {
+ 
+    var _location = "welcome"
+    
+    switch (_language_detected.substring(0, 2)) {
+      case("es"):
+        _location = "inicio"
+        break
+      case("it"):
+        _location = "inicio"
+        break
+      default:
+        break
+    }
+    alert("navigate to:" + _location)
+  }
+    
 })()
